3d96d606447cb49226d9b1309c33d3e0b03cbab5
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2025-05-25 20:51:34 +0530

    add password protection

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import App from './App.svelte'
+import App from "./App.svelte";
+const password_env = import.meta.env.VITE_PASSWORD;
+
+const password = prompt("Enter password:");
+if (password !== password_env) {
+  alert("Incorrect password. Access denied.");
+  throw new Error("Unauthorized");
+}
 
 const app = new App({
-  target: document.getElementById('app')
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
